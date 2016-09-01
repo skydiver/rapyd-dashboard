82cbd242c741bdf3f896a17be40c394fae1c332b
Martin <skydiver@users.noreply.github.com>
2016-09-01 14:45:59 -0300

    fix for file upload forms

--- views/rapyd/form_raw.blade.php
+++ views/rapyd/form_raw.blade.php
@@ -3,7 +3,7 @@
     <div class="box-header with-border">
         <h3 class="box-title">{{ $title }}</h3>
     </div>
-    <form id="rapyd-form" action="?process=1" role="form" method="post" class="form-horizontal">
+    <form id="rapyd-form" action="?process=1" role="form" method="post" class="form-horizontal"{!! (isset($formdata) && $formdata == true) ? ' enctype="multipart/form-data"' : '' !!}>
         {!! $form !!}
     </form>
 </div>
