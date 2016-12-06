fa1c6f5e7f5f33e2624c7c4c5c08bea6131a3293
Martin <skydiver@users.noreply.github.com>
2016-12-05 22:02:28 -0300

    better session messages

--- views/template/session_message.blade.php
+++ views/template/session_message.blade.php
@@ -3,7 +3,18 @@
         <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
     @if(Session::get('message')['type'] == 'success')
         <i class="icon fa fa-check"></i>
+    @elseif(Session::get('message')['type'] == 'danger')
+        <i class="icon fa fa-ban"></i>
+    @elseif(Session::get('message')['type'] == 'warning')
+        <i class="icon fa fa-warning"></i>
     @endif
         <strong>{!! Session::get('message')['msg'] !!}</strong>
+    @if(isset(Session::get('message')['list']) && is_array(Session::get('message')['list']))
+        <ul>
+        @foreach(Session::get('message')['list'] as $item)
+            <li>{{ $item }}</li>
+        @endforeach
+        </ul>
+    @endif
     </div>
 @endif
