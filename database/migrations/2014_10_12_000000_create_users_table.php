b3aac57f842156dfbf7e271535bbebe560d2e34e
Martin <skydiver@users.noreply.github.com>
2016-12-21 21:52:45 -0300

    migration fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -9,9 +9,9 @@
 
             Schema::create('users', function (Blueprint $table) {
                 $table->increments('id');
-                $table->string('name');
-                $table->string('email')->unique();
-                $table->string('password', 60);
+                $table->string('name'    ,  80);
+                $table->string('email'   , 100)->unique();
+                $table->string('password',  60)->nullable();
                 $table->rememberToken();
                 $table->timestamps();
             });
